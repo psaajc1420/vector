476e08648cf26cd4804343abf15262af410ff9e7
psaajc1420 <psaajc1420@yahoo.com>
2021-10-18 13:11:34 -0500

    style: add better tests and prettier output

--- main.cpp
+++ main.cpp
@@ -39,29 +39,47 @@ void PrintVector(const library::Vector<int> &vec) {
 
 int main() {
 
+  std::cout << "Bubble Sort" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
   library::Vector<int> nums = {4, 5, 10, 1, 3, 45, 2};
   PrintVector(nums);
   BubbleSort(nums.Begin(), nums.End());
   PrintVector(nums);
+  std::cout << std::endl;
 
+  std::cout << "Merge Sort" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
   nums = {4, 5, 10, 1, 3, 45, 2};
   PrintVector(nums);
   MergeSort(nums.Begin(), nums.End());
   PrintVector(nums);
+  std::cout << std::endl;
 
+  std::cout << "Standard Library Sort" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
+  nums = {4, 5, 10, 1, 3, 45, 2};
+  PrintVector(nums);
+  std::sort(nums.Begin(), nums.End());
+  PrintVector(nums);
+  std::cout << std::endl;
+
+  std::cout << "Find" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
   auto it1 = Find(45, nums.Begin(), nums.End());
   auto it2 = Find(1, nums.Begin(), nums.End());
-
   it1 = nums.Begin() + (it1 - it2) / 2;
-
   std::cout << *it1 << std::endl;
+  std::cout << std::endl;
 
+  std::cout << "Const Iterator Functionality" << std::endl;
+  std::cout << "---------------------------------" << std::endl;
   library::Vector<std::string> words = {"hello", "world"};
   using ConstIterator = typename library::Vector<std::string>::const_iterator;
   for (ConstIterator it = words.cBegin(); it != words.cEnd(); ++it) {
     std::cout << *it << " ";
   }
   std::cout << std::endl;
+
 
   return 0;
 }
